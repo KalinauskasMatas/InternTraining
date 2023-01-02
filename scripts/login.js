d61ac3d304bd241673dc24e1626a73c6fb7c11eb
Matas Kalinauskas <matas.kalinauskas@accenture.com>
2023-01-02 19:52:58 +0200

    add login validation with redirect

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,19 @@
+const loginEl = document.getElementsByClassName('login-form')[0];
+const loginSubmitEl = document.querySelector('.login-form .form-button');
 const registerEl = document.getElementsByClassName('register-form')[0];
 const showRegisterEl = document.getElementById('show-register');
+
+const signIn = (e) => {
+  if(loginEl.checkValidity()){
+    e.preventDefault();
+    location.href="./home.html";
+  } 
+}
 
 const showRegisterForm = () => {
   registerEl.style.display = 'flex';
   showRegisterEl.style.display = 'none';
 }
 
+loginSubmitEl.addEventListener('click', signIn);
 showRegisterEl.addEventListener('click', showRegisterForm);
