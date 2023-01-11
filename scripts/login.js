57e38c52b832403f87388e69507cea22d432aab5
Matas Kalinauskas <matas.kalinauskas@accenture.com>
2023-01-11 10:25:29 +0200

    add registeredUsers localstorage, add users after registration

--- scripts/login.js
+++ scripts/login.js
@@ -1,41 +1,56 @@
-const loginEl = document.getElementsByClassName('login-form')[0];
-const loginSubmitEl = document.querySelector('.login-form .form-button');
-const registerEl = document.getElementsByClassName('register-form')[0];
-const registerEmailEl = document.getElementById('email-form');
-const registerEmailRepeatEl = document.getElementById('email-repeat');
-const registerPasswordEl = document.getElementById('password-form');
-const registerPasswordRepeatEl = document.getElementById('password-repeat');
-const registerSubmitEl = document.querySelector('.register-form .form-button');
-const showRegisterEl = document.getElementById('show-register');
+const loginEl = document.getElementsByClassName("login-form")[0];
+const loginSubmitEl = document.querySelector(".login-form .form-button");
+const registerEl = document.getElementsByClassName("register-form")[0];
+const fNameEl = document.getElementById("fname");
+const surnameEl = document.getElementById("surname");
+const registerEmailEl = document.getElementById("email-form");
+const registerEmailRepeatEl = document.getElementById("email-repeat");
+const registerPasswordEl = document.getElementById("password-form");
+const registerPasswordRepeatEl = document.getElementById("password-repeat");
+const registerSubmitEl = document.querySelector(".register-form .form-button");
+const showRegisterEl = document.getElementById("show-register");
+
+const registeredUsers =
+  JSON.parse(localStorage.getItem("registeredUsers")) || [];
 
 const signIn = (e) => {
-  if(loginEl.checkValidity()){
+  if (loginEl.checkValidity()) {
     e.preventDefault();
-    location.href="./home.html";
-  } 
-}
+    location.href = "./home.html";
+  }
+};
 
 const register = (e) => {
-  registerEmailRepeatEl.setCustomValidity('');
-  registerPasswordRepeatEl.setCustomValidity('');
+  registerEmailRepeatEl.setCustomValidity("");
+  registerPasswordRepeatEl.setCustomValidity("");
 
-  if(registerEmailEl.value !== registerEmailRepeatEl.value)
-    registerEmailRepeatEl.setCustomValidity('Email addresses do not match');
+  if (registerEmailEl.value !== registerEmailRepeatEl.value)
+    registerEmailRepeatEl.setCustomValidity("Email addresses do not match");
 
-  if(registerPasswordEl.value !== registerPasswordRepeatEl.value)
-    registerPasswordRepeatEl.setCustomValidity('The passwords do not match');
+  if (registerPasswordEl.value !== registerPasswordRepeatEl.value)
+    registerPasswordRepeatEl.setCustomValidity("The passwords do not match");
 
-  if(registerEl.checkValidity()) {
-    e.preventDefault();
-    location.href="./home.html";
-  }
-}
+  if (!registerEl.checkValidity()) return;
+
+  e.preventDefault();
+
+  registeredUsers.push({
+    name: fNameEl.value,
+    surname: surnameEl.value,
+    email: registerEmailEl.value,
+    password: registerPasswordEl.value,
+  });
+
+  localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
+
+  location.href = "./home.html";
+};
 
 const showRegisterForm = () => {
-  registerEl.style.display = 'flex';
-  showRegisterEl.style.display = 'none';
-}
+  registerEl.style.display = "flex";
+  showRegisterEl.style.display = "none";
+};
 
-loginSubmitEl.addEventListener('click', signIn);
-registerSubmitEl.addEventListener('click', register);
-showRegisterEl.addEventListener('click', showRegisterForm);
+loginSubmitEl.addEventListener("click", signIn);
+registerSubmitEl.addEventListener("click", register);
+showRegisterEl.addEventListener("click", showRegisterForm);
