5b815e2356d82e4a3c3bb1cfd392670f4c2dec34
Matas Kalinauskas <matas.kalinauskas@accenture.com>
2023-01-13 18:48:08 +0200

    add redux

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./Footer.css";
+
 const Footer = () => {
-  return <p>Footer</p>;
+  return (
+    <footer>
+      <span>Made by: Matas Kalinauskas</span>
+      <span>2023</span>
+    </footer>
+  );
 };
 
 export default Footer;
