a1dea13a33391aad7a11b2407adcb7aafb0681b2
Matas Kalinauskas <matas.kalinauskas@accenture.com>
2023-01-27 15:22:28 +0200

    remove unused localstorage calls

--- src/redux/features/currentUser/currentUserSlice.ts
+++ src/redux/features/currentUser/currentUserSlice.ts
@@ -11,15 +11,11 @@ const currentUserSlice = createSlice({
   reducers: {
     setCurrUser: (state, action: PayloadAction<UserState>) => {
       Object.assign(state, action.payload);
-      // localStorage.setItem("currUser", JSON.stringify(state));
     },
     updateCurrUser: (state, action: PayloadAction<UserState>) => {
       Object.assign(state, action.payload);
-
-      // localStorage.setItem("currUser", JSON.stringify(state));
     },
     clearUser: (state) => {
-      // localStorage.removeItem("currUser");
       state = { ...initialState };
     },
   },
